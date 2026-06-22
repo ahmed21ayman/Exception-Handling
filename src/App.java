99461000e99809a6256ed5eac5804d2118e9dfa6
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-22 21:55:07 +0300

    v1: normal try catch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        try{
+            int a= 100/0;
+            int b=10;
+            System.out.println(a/b);
+        }catch(Exception e){
+            System.out.println("error : " + e.getMessage());
+        }
     }
 }
