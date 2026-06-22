034769e15a0750bfdb7d15ac49f78573a252f36c
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-22 21:56:34 +0300

    v2: try catch type of error

--- src/App.java
+++ src/App.java
@@ -7,5 +7,14 @@ public class App {
         }catch(Exception e){
             System.out.println("error : " + e.getMessage());
         }
+
+        try{
+            int a= 100/0;
+            int b=10;
+            System.out.println(a/b);
+        }catch(ArithmeticException e){
+            System.out.println("error Arithmetic: " + e.getMessage());
+        }
+
     }
 }
